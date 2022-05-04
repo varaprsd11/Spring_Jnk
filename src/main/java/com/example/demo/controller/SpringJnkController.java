9b1fc151730d18227478912758bdbd78bbd3e680
varaprsd11 <varaprsd11@gmail.com>
2022-05-04 17:25:45 +0530

    second commit

--- src/main/java/com/example/demo/controller/SpringJnkController.java
+++ src/main/java/com/example/demo/controller/SpringJnkController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,9 @@ public class SpringJnkController {
 	
 	public static org.slf4j.Logger logger= LoggerFactory.getLogger(SpringJnkController.class);
 	
+	@GetMapping("/test")
 	public String getMessage() {return "message sent";}
+	
+	@GetMapping("/sent")
+	public String sentMessage() {return "recived message";}
 }
